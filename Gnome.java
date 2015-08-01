51c163be1c941c54ae6df482b97e21e18a1494c4
yingyuyang <yyyayst@gmail.com>
2015-07-31 20:39:03 -0400

    Update Gnome.java

--- Gnome.java
+++ Gnome.java
@@ -1,32 +1,102 @@
+import java.util.LinkedList;
 
-public class Gnome {
-	private Village finalV;
-	private Village currentV;
-	private int Urgency = 0;
+public class Gnome implements Runnable {
+	private static final int STARTING_ID = 10000;
+	private static int numMade = 0;
+	private int id;
+	private String name;
+	private boolean Urgent = false; 
+	//False means the gnome is in a lazy mode. True means in an urgent(desperate) mode. 
+
+	private Village finalV; //the final destination for this gnome
+	private Village currentV;//the current village the the gnome resides 
+	private LinkedList<Village> visitedV; //a history of the places the gnome has visited
+	private Graph graph;
+	private DAlg d = new DAlg();
 	
-	public void setfinalV(Village x){
-		finalV = x;
-	}
-	public Village getfinalV(){
-		return finalV;
-	}
-	public void setcurrentV(Village x){
-		currentV = x;
-	}
-	public Village getcurrentV(){
-		return currentV;
-	}
-	public void setCurrency(int x){
-		Urgency = 0;
-	}
-	public int getCurrency(){
-		return Urgency;
+	/**Create a unique ID for each Gnome, starting from 10000.*/
+	private int createID() {
+		return STARTING_ID + numMade++;
 	}
 	
+	//setters and getters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name; 
+	}
+	
+	public void setUrgency(boolean x) {
+		this.Urgent = x;
+	}
+	public boolean getUrgency() {
+		return this.Urgent;
+	}
+	
+	public void setfinalV(Village x) {
+		this.finalV = x;
+	}
+	public Village getfinalV() {
+		return this.finalV;
+	}
+	
+	public void setcurrentV(Village x) {
+		this.currentV = x;
+	}
+	public Village getcurrentV() {
+		return this.currentV;
+	}
+	
+	//Constructors
+	/**Constr: An instance of Gnome. */
+	public Gnome() {
+	}
+	
+	/**Constr: An instance of Gnome with a name and a unique ID.*/
+	public Gnome(String name) {
+		this.name = name;
+		setId(createID());
+	}
+	
+	/**Constr: An instance of Gnome with name, ID, final Village and current Village.*/
+	public Gnome(String name, Village fv, Village cv) {
+		this.name = name;
+		this.finalV = fv;
+		this.currentV = cv;
 	}
 
+	/**Constr: An instance of Gnome with name, ID, final/current Village and a mode.*/
+	public Gnome(String name, Village fv, Village cv, boolean mode, Graph graph) {
+		this.name = name;
+		this.finalV = fv;
+		this.currentV = cv;
+		this.Urgent = mode;
+		this.graph = graph;
+		this.d = new DAlg(graph);
+	}
+	
+	@Override
+	public void run() {
+		if (this.Urgent) {
+			//choose the shortest path
+			visitedV = d.path(currentV, finalV);
+			for (int i = 0; i < visitedV.size(); i++) {
+				visitedV.get(i).setData(this);
+				System.out.println("The village that gnome " + this.name + " has past is "
+						+ visitedV.get(i));
+			}
+		} else {
+			//choose a random path
+			System.out.println("Here is a random path. ");
+			
+		}
+	}
 }
