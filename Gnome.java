c31c3676b01d332d438c45b2f523ecda0cf505d9
yingyuyang <yyyayst@gmail.com>
2015-08-02 14:41:36 -0400

    Added a searchGnome function

--- Gnome.java
+++ Gnome.java
@@ -1,19 +1,22 @@
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Gnome implements Runnable {
 	//private static final int STARTING_ID = 10000;
 	//private static int numMade = 0;
 	//private int id;
 	private String name;
-	
+
 	private Village currentV;
 	private Village finalV;
 	private ArrayList<Village> history;
-	
+
 	private Graph graph;
 	private boolean urgency;
-	
+	//create a hashMap for holding all the gnomes. 
+	static private HashMap<String, Gnome> hm= new HashMap<String, Gnome>();
+
 	//constructor
 	public Gnome(String name, Village currentV, Graph graph){
 		this.name = name;
@@ -22,6 +25,7 @@ public class Gnome implements Runnable {
 		this.history = new ArrayList<Village>();
 		this.graph = graph;
 		this.urgency = false;
+		hm.put(name, this);
 	}
 	public Gnome(String name, Village currentV, Village finalV, Graph graph){
 		this.name = name;
@@ -30,14 +34,13 @@ public class Gnome implements Runnable {
 		this.history = new ArrayList<Village>();
 		this.graph = graph;
 		this.urgency = false;
+		hm.put(name, this);
 	}
-	
-	public void setname(String string){
-		name = string;
-	}
-	public String getname(){
-		return name;
-	}
+
+
+	//private int createID() {
+	// return STARTING_ID + numMade++;
+	//}
 	public void setfinalV(Village x){
 		finalV = x;
 	}
@@ -56,79 +59,59 @@ public class Gnome implements Runnable {
 	public boolean getUrgency(){
 		return urgency;
 	}
+	public String getName() {
+		return this.name;
+	}
+	public ArrayList<Village> getHistory() {
+		return this.history;
+	}
 	/***Moves Gnome directly to final destination via shortest path*/
 	public void haulass(){
 		DAlg DAlg = new DAlg(graph);
 		LinkedList<Village> path = DAlg.path(currentV, finalV);
-			try {
-				for (Village x: path){
+		try {
+			for (Village x: path){
 				Thread.sleep(1000);
 				currentV = x;
 				currentV.printVillage();
 				history.add(currentV);
-				}
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-		}	
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
-	
+
 	public void run() {
 		while (finalV == null){
 			try {
 				Thread.sleep(1000);
 				int number = currentV.getoutgoing().size();
-				if (number == 0){
-					currentV = currentV;
-				}
-				else{
-					int randomNum = (int) (number*Math.random()+0);
-					currentV = currentV.getoutgoing().get(randomNum);					history.add(currentV);
-					//System.out.print(name + "is at: ");
-					currentV.printVillage();
-					}
-				}
+				int randomNum = (int) (number*Math.random()+0);
+				currentV = currentV.getoutgoing().get(randomNum);     
+				history.add(currentV);
+				currentV.printVillage();
+			}
 			catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		if(urgency == false){
-			for (int i = 0; i < 5;i++){
-				try {
-					Thread.sleep(1000);
-					int number = currentV.getoutgoing().size();
-					if (number == 0){
-						currentV = currentV;
-					}
-					else{
-						int randomNum = (int) (number*Math.random()+0);
-						currentV = currentV.getoutgoing().get(randomNum);					history.add(currentV);
-						//System.out.print(name + "is at: ");
-						currentV.printVillage();
-						}
-					}
-				catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				}
-		}
 		haulass();
 	}
-	
+
 	public static void main(String[] args) {
 		/////////////////////////////////////////////////////////////////////////////////////
 		///////////////////INITIALIZES EVERYTHING////////////////////////////////////////////
 		/////////////////////////////////////////////////////////////////////////////////////
 		//Test Villages
-		Village Village0 = new Village(0, 0);
-		Village Village1 = new Village(1, 1);
-		Village Village2 = new Village(2, 2);
-		Village Village3 = new Village(3, 3);
-		Village Village4 = new Village(4, 4);
-		Village Village5 = new Village(5, 5);
-		
+		Village Village0 = new Village(0);
+		Village Village1 = new Village(1);
+		Village Village2 = new Village(2);
+		Village Village3 = new Village(3);
+		Village Village4 = new Village(4);
+		Village Village5 = new Village(5);
+
 		//Test Roads
 		Road Road01 = new Road("Road 0-1", Village0, Village1, 5, 0);
 		Road Road10 = new Road("Road 1-0", Village1, Village0, 5, 0);
@@ -144,7 +127,7 @@ public class Gnome implements Runnable {
 		Road Road51 = new Road("Road 5-1", Village5, Village1, 2, 0);
 		Road Road25 = new Road("Road 2-5", Village2, Village5, 5, 10);
 		Road Road52 = new Road("Road 5-5", Village5, Village2, 5, 10);
-		
+
 		//Test Graph
 		ArrayList<Village> Villages = new ArrayList<Village>();
 		Villages.add(Village0);
@@ -153,7 +136,7 @@ public class Gnome implements Runnable {
 		Villages.add(Village3);
 		Villages.add(Village4);
 		Villages.add(Village5);
-		
+
 		ArrayList<Road> Roads = new ArrayList<Road>();
 		Roads.add(Road01);
 		Roads.add(Road10);
@@ -169,16 +152,38 @@ public class Gnome implements Runnable {
 		Roads.add(Road51);
 		Roads.add(Road25);
 		Roads.add(Road52);
-		
+
 		Graph graph = new Graph(Villages, Roads);
 		/////////////////////////////////////////////////////////////////////////////////////
 		///////////////////INITIALIZES EVERYTHING////////////////////////////////////////////
 		/////////////////////////////////////////////////////////////////////////////////////
 
 		Gnome Gnome1 = new Gnome("Gnome1", Village0, graph);
-		//Gnome1.setUrgency(true);
-		Gnome1.setfinalV(Village5);
+		Gnome1.setfinalV(Village4);
 		Thread Thread1 = new Thread(Gnome1); 
 		Thread1.start();
+
+//		Gnome Gnome2 = new Gnome("Gnome2", Village5, graph);
+//		Gnome2.setfinalV(Village4);
+//		Thread Thread2 = new Thread(Gnome2); 
+//		Thread2.start();
+		
+		try {
+			Thread.sleep(5000);
+			searchGnome(Gnome1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**Search for a given gnome, locating their current position and acquiring a history
+	 * of the places that this gnome has visited. Use HashMap. */
+	public static void searchGnome(Gnome g) {
+		String key = g.getName();
+		Gnome findG = hm.get(key);
+		System.out.println("The gnome " + g.getName() + " is currently in " + 
+				findG.getcurrentV());
+		System.out.println("And this gnome has visited: " + g.getHistory());
 	}
 }
